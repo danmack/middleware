99ffd111421edd48a5fde8182f3e104179c033a5
Brian Meagher <brian.meagher@ixsystems.com>
2024-05-16 19:00:48 -0700

    Add test__client_objects to detect and close Client objects

--- tests/api2/test_zzz_tests_completed.py
+++ tests/api2/test_zzz_tests_completed.py
@@ -1,5 +1,12 @@
 import faulthandler
+import gc
 import threading
+
+from middlewared.client import Client
+
+
+def objects(cls):
+    return [obj for obj in gc.get_objects() if isinstance(obj, cls)]
 
 
 def test__thread_count(request):
@@ -10,3 +17,14 @@ def test__thread_count(request):
         with open('threads.trace', 'w') as f:
             faulthandler.dump_traceback(f)
         assert count == 1
+
+
+def test__client_objects(request):
+    """Check whether any Client objects still exist, and attempt to close them if so."""
+    objs = objects(Client)
+    for obj in objs:
+        try:
+            obj.close()
+        except Exception:
+            pass
+    assert 0 == len(objs)
