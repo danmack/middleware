eba3e433a2ca29829f5ce5c9ff20162248b76be4
Mark Grimes <mark.grimes@ixsystems.com>
2024-07-05 15:35:36 -0700

    Debugging.

--- tests/api2/test_nfs_xattr_support.py
+++ tests/api2/test_nfs_xattr_support.py
@@ -1,4 +1,5 @@
-import pytest
+import contextlib
+# import pytest
 
 from auto_config import password, pool_name, user
 from middlewared.test.integration.assets.pool import dataset as nfs_dataset
@@ -7,7 +8,8 @@ from middlewared.test.integration.utils.client import truenas_server
 from protocols import SSH_NFS, nfs_share
 
 
-@pytest.fixture(scope="module")
+# @pytest.fixture(scope="module")
+@contextlib.contextmanager
 def start_nfs():
     """ The exit state is managed by init_nfs """
     try:
@@ -16,6 +18,7 @@ def start_nfs():
         call('service.stop', 'nfs', {'silent': False})
 
 
+# def test_nfs_xattr_support(start_nfs):
 def test_nfs_xattr_support(start_nfs):
     """
     Perform basic validation of NFSv4.2 xattr support.
@@ -28,14 +31,15 @@ def test_nfs_xattr_support(start_nfs):
     # with nfs_dataset("test_nfs4_xattr", mode="777", delete_delay=10):
     with nfs_dataset("test_nfs4_xattr", mode="777"):
         with nfs_share(xattr_nfs_path):
-            with SSH_NFS(truenas_server.ip, xattr_nfs_path, vers=4.2,
-                         user=user, password=password, ip=truenas_server.ip) as n:
-                n.create("testfile")
-                n.setxattr("testfile", "user.testxattr", "the_contents")
-                xattr_val = n.getxattr("testfile", "user.testxattr")
-                assert xattr_val == "the_contents"
+            with start_nfs():
+                with SSH_NFS(truenas_server.ip, xattr_nfs_path, vers=4.2,
+                             user=user, password=password, ip=truenas_server.ip) as n:
+                    n.create("testfile")
+                    n.setxattr("testfile", "user.testxattr", "the_contents")
+                    xattr_val = n.getxattr("testfile", "user.testxattr")
+                    assert xattr_val == "the_contents"
 
-                n.create("testdir", True)
-                n.setxattr("testdir", "user.testxattr2", "the_contents2")
-                xattr_val = n.getxattr("testdir", "user.testxattr2")
-                assert xattr_val == "the_contents2"
+                    n.create("testdir", True)
+                    n.setxattr("testdir", "user.testxattr2", "the_contents2")
+                    xattr_val = n.getxattr("testdir", "user.testxattr2")
+                    assert xattr_val == "the_contents2"
